d4f078abcbeea614ac2b32808c9cadf460a61e2e
Campbell Barton <ideasman42@gmail.com>
2022-02-11 16:48:07 +1100

    Cleanup: use brief SPDX license

--- undo-fu-session.el
+++ undo-fu-session.el
@@ -1,5 +1,6 @@
 ;;; undo-fu-session.el --- Persistent undo, available between sessions -*- lexical-binding: t -*-
 
+;; SPDX-License-Identifier: GPL-2.0-or-later
 ;; Copyright (C) 2020  Campbell Barton
 ;; Copyright (C) 2009-2015  Tomohiro Matsuyama
 
@@ -9,19 +10,6 @@
 ;; Keywords: convenience
 ;; Version: 0.2
 ;; Package-Requires: ((emacs "24.1"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
